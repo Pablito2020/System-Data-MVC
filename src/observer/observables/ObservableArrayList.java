52fa246fd4dcc01e18d791f1ceac1d750062b458
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2020-07-23 09:59:00 +0200

    Observable with Array List implementation

---
+++ src/observer/observables/ObservableArrayList.java
@@ -0,0 +1,36 @@
+package observer.observables;
+
+import observer.observers.Observer;
+
+import java.util.ArrayList;
+
+public class ObservableArrayList implements Observable {
+
+    private ArrayList<Observer> observers;
+
+    public ObservableArrayList() {
+        this.observers = new ArrayList<>();
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void deleteObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for(Observer observer : observers)
+            observer.update();
+    }
+
+    @Override
+    public void deleteObserver(int index) {
+        observers.remove(index);
+    }
+
+}
